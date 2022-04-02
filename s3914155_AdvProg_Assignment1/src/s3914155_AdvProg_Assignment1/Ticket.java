31d5ce799ea55e9e80010147bfbc5c9ac9dea935
katecxh <102843555+katecxh@users.noreply.github.com>
2022-04-02 18:52:51 +1100

    Update Ticket.java

--- s3914155_AdvProg_Assignment1/src/s3914155_AdvProg_Assignment1/Ticket.java
+++ s3914155_AdvProg_Assignment1/src/s3914155_AdvProg_Assignment1/Ticket.java
@@ -3,19 +3,20 @@ package s3914155_AdvProg_Assignment1;
 abstract class Ticket {
     public String time, zone;
 	public double cost;
+ // The Ticket class is a parent class which the child classes inherit constructors, methods, and attributes.
 
     public Ticket(String time, String zone, double cost) {
     	this.time = time;
     	this.zone = zone;
     	this.cost = cost;
     }
-    abstract public void getHistory();
-
 	public void settime(String time) {
 		this.time = time;
+ // This setter method appends the value of a user's purchase to the Ticket abstract class 
 	}
 	public String gettime() { 
 		return time;
+ // This getter method returns the value of a user's purchase to the Ticket abstract class 
 	}
 	public void setzone(String zone) {
 		this.zone = zone;
@@ -29,4 +30,3 @@ abstract class Ticket {
 	public double getcost() { 
 		return cost;
 	}}
-
